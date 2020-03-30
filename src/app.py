3eb86a442313c408d24dc8940fe28f03c0cbf6a1
LucileRR <lu.rainteau@gmail.com>
2020-03-30 20:00:02 +0200

    Update for final project

--- src/app.py
+++ src/app.py
@@ -8,9 +8,12 @@ import pandas as pd
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+
 from dash.dependencies import Input, Output
 
-
+from scipy.integrate import solve_ivp
+from scipy.optimize import minimize
 
 #Répertoire du fichier de données
 PROCESSED_DIR = '../data/processed/'
@@ -35,7 +38,7 @@ epidemie_df = (pd.read_csv(DATA_FILE, parse_dates=["Last Update"])
                [lambda df: df.day <= datetime.date(2020, 3,10)]
               )
 
-countries = [{'label': c, 'value': c} for c in epidemie_df['Country/Region'].unique()]
+countries = [{'label': c, 'value': c} for c in epidemie_df['Country/Region'].unique()]       
 
 app = dash.Dash('Covid-19 Explorer')
 
@@ -80,6 +83,41 @@ app.layout = html.Div([
                 marks={i:str(i) for i, date in enumerate(epidemie_df['day'].unique())}
             )  
         ]),
+        dcc.Tab(label='Model SIR', children=[
+            html.Div([
+                dcc.Dropdown(
+                    id='country_model',
+                    options=countries
+                )
+            ]),
+            html.Div([
+                dcc.Input(
+                    id='beta_input',
+                    placeholder='Enter a value for beta...',
+                    type='number'
+                ),
+                dcc.Input(
+                    id='gamma_input',
+                    placeholder='Enter a value for gamma...',
+                    type='number'
+                ),
+                dcc.Input(
+                    id='pop_input',
+                    placeholder='Enter a value for population...',
+                    type='number'
+                ),
+                dcc.Checklist(
+                    id='opt_parameters',
+                    options=[
+                        {'label': 'Use optimal parameters', 'value': 'Y'}
+                    ],
+                    value=[]
+                ),
+            ]),
+            html.Div([
+                dcc.Graph(id='graph_model')
+            ]),
+        ]),
     ]),
 ])
 
@@ -107,7 +145,6 @@ def update_graph(country, country2, variable):
                     .agg({variable: 'sum'})
                     .reset_index()
                    )
-
     
     return {
         'data': [
@@ -161,6 +198,122 @@ def update_map(map_day):
     }
 
 
+# Fonction pour le modèle SIR
+def SIR(t, y):
+    S = y[0]
+    I = y[1]
+    R = y[2]
+    return([-beta*S*I, beta*S*I-gamma*I, gamma*I])
 
+
+# Fonction d'optimisation des paramètres du modèle SIR
+def sumsq_error(parameters):
+    beta, gamma = parameters
+    
+    def SIR(t, y):
+        S = y[0]
+        I = y[1]
+        R = y[2]
+        return([-beta*S*I, beta*S*I-gamma*I, gamma*I])
+
+    solution = solve_ivp(SIR, [0, nb_steps-1], [pop_total, 1, 0], t_eval=np.arange(0, nb_steps, 1))
+    
+    return(sum((solution.y[1]-infected_population)**2))
+
+
+
+@app.callback(
+    Output('graph_model', 'figure'),
+    [
+        Input('country_model', 'value'),
+        Input('beta_input', 'value'),
+        Input('gamma_input', 'value'),
+        Input('pop_input', 'value'),
+        Input('opt_parameters', 'value')
+    ]
+)
+def update_model(country_model, beta_input, gamma_input, pop_input, opt_parameters):
+    global beta
+    global gamma
+    global nb_steps
+    global pop_total
+    global infected_population
+    
+    pop_total = pop_input
+    
+    if beta_input is None:
+        beta = 0.01
+    else:
+        beta = beta_input
+        
+    if gamma_input is None:
+        gamma = 0.1
+    else:
+        gamma = gamma_input
+        
+    if country_model is None:
+        country_df = (epidemie_df
+                      .groupby('day')
+                      .agg({'Confirmed': 'sum', 'Deaths': 'sum', 'Recovered': 'sum'})
+                      .reset_index()
+                     )
+    else:
+        country_df = (epidemie_df[epidemie_df['Country/Region'] == country_model]
+                      .groupby(['Country/Region', 'day'])
+                      .agg({'Confirmed': 'sum', 'Deaths': 'sum', 'Recovered': 'sum'})
+                      .reset_index()
+                     )
+        
+    country_df['infected'] = country_df['Confirmed'].diff()
+    nb_steps = country_df.shape[0]
+    infected_population = country_df['infected'] 
+
+    
+    if 'Y' in opt_parameters and pop_total is not None:
+        msol = minimize(sumsq_error, [beta, gamma], method='Nelder-Mead')
+        print(msol.x)
+        beta, gamma = msol.x
+        solution = solve_ivp(SIR, [0, nb_steps-1], [0.00001*pop_total, 1, 0], t_eval=np.arange(0, nb_steps, 1))
+
+    
+    if pop_total is not None:
+        solution = solve_ivp(SIR, [0, nb_steps-1], [0.00001*pop_total, 1, 0], t_eval=np.arange(0, nb_steps, 1))
+    #plot_epidemia(solution, country_df.loc[2:]['infected'])
+    
+    return{
+        'data': ([
+            dict(
+                x=solution.t,
+                y=solution.y[0],
+                type='line',
+                name="Susceptible"
+            )
+        ] if pop_input is not None else []) +
+        ([
+            dict(
+                x=solution.t,
+                y=solution.y[1],
+                type='line',
+                name="Infected"
+            )
+        ] if pop_input is not None else []) +
+        ([
+            dict(
+                x=solution.t,
+                y=solution.y[2],
+                type='line',
+                name="Recovered"
+            )
+        ] if pop_input is not None else []) +
+        [
+            dict(
+                x=country_df.loc[2:]['infected'].reset_index(drop=True).index,
+                y=country_df.loc[2:]['infected'],
+                type='line',
+                name="Original Data"
+            )
+        ]
+    }
+    
 if __name__ == '__main__':
     app.run_server(debug=True)
